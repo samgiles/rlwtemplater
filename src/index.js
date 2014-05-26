8a2cac1b612ee0d772d021471ad817934c44a881
SamGiles <aphro.cag@googlemail.com>
2014-05-26 13:30:35 +0100

    Initial commit with concept

---
+++ src/index.js
@@ -0,0 +1,43 @@
+/**
+ * Really light weight templating. Designed to have a small
+ * footprint and have the ability to precompile to a pure javascript
+ * function if you so wish by calling toString() on the result of
+ * calling compile();
+ *
+ * Syntax is relatively simple.  If you want to inline a value:
+ *
+ * ```
+ *		var template = Template.compile("<+ data.value +> blah blah");
+ *		var rendered = template({value: "zzzz..."});
+ * ```
+ *
+ * Rendered will be: "zzzz... blah blah";
+ *
+ * If you want to use javascript control structures:
+ *
+ * ```
+ *		var template = Template.compile("<| for (var x in data) { |> Hello <+x+> <| } |>");
+ *		var rendered = template({ "Jane": "Person 1", "Bill": "Person 2"});
+ * ```
+ *
+ * Rendered will be: "Hello Jane Hello Bill";
+ *
+ * Notice how the control structure must be syntactically correct Javascript
+ * and notice how it must be enclosed within <| |> braces.
+ *
+ * MIT Licenced - Samuel Giles http://samgil.es
+ */
+
+function js(s) {
+	return "p('" + s.replace(/'/g, "\\'").
+		replace(/<\+/g, "');p(").
+		replace(/\+>/g, ");p('").
+		replace(/<\|/g, "');").
+		replace(/\|>/g, "p('").
+		replace(/\\/g, "\\\\").
+		replace(/\n/gi, "") + "');";
+}
+
+module.exports.compile = function(s) {
+	return new Function("data", "var b=[],p=b.push;" + js(s) + "return b.join('');");
+};
